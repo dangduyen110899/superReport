a766dba526b3d793109aeef0d7580a0779fabaa5
dangduyen110899 <duyendt@fimo.edu.vn>
2021-03-30 23:39:24 +0700

    phan quyen

--- back/src/router/router.js
+++ back/src/router/router.js
@@ -12,40 +12,40 @@ const verifySignUp = require('../router/verifySignUp');
 function route(app) {
 
 //  app.get('/api/lecturer/hehe',function(res,req) {lecturer.hehe});
-app.get('/api/admin/lecturer', [authJwt.verifyToken, authJwt.isAdmin],lecturer.list);
+app.get('/api/admin/lecturer', [authJwt.verifyToken, authJwt.isLeaderOrAdmin],function () {
+    lecturer.list
+});
 app.post('/api/admin/lecturer/create',[authJwt.verifyToken, authJwt.isAdmin],lecturer.create);
 app.post('/api/admin/lecturer/creates', [authJwt.verifyToken, authJwt.isAdmin],upload.single("file"),lecturer.creates);
 app.put('/api/admin/lecturer/update',[authJwt.verifyToken, authJwt.isAdmin],lecturer.update);
 
-app.get('/api/admin/student', [authJwt.verifyToken, authJwt.isAdmin],student.list);
+app.get('/api/admin/student', [authJwt.verifyToken, authJwt.isLeaderOrAdmin],student.list);
 app.post('/api/admin/student/create', [authJwt.verifyToken, authJwt.isAdmin],student.create);
 app.post('/api/admin/student/creates', [authJwt.verifyToken, authJwt.isAdmin],upload.single("file"),student.creates);
 app.put('/api/admin/student/update', [authJwt.verifyToken, authJwt.isAdmin],student.update);
 
-app.get('/api/admin/thesis', [authJwt.verifyToken, authJwt.isAdmin],thesis.list);
+app.get('/api/admin/thesis', [authJwt.verifyToken, authJwt.isAdmin], function () {
+    isLeaderOrAdmin.list
+});
 // app.post('/api/admin/thesis/create',thesis.create);
 app.post('/api/admin/thesis/creates', [authJwt.verifyToken, authJwt.isAdmin],upload.single("file"),thesis.creates);
 app.post('/api/admin/thesis/checkYear', [authJwt.verifyToken, authJwt.isAdmin],thesis.checkYear);
 app.put('/api/admin/thesis/update', [authJwt.verifyToken, authJwt.isAdmin],thesis.update);
 
 
-app.get('/api/admin/tkb', [authJwt.verifyToken, authJwt.isAdmin],subSubjectLecturer.list);
+app.get('/api/admin/tkb', [authJwt.verifyToken, authJwt.isLeaderOrAdmin],  function () {
+    subSubjectLecturer.list
+} );
 app.post('/api/admin/tkb/create', [authJwt.verifyToken, authJwt.isAdmin],subSubjectLecturer.create);
 app.post('/api/admin/tkb/creates', [authJwt.verifyToken, authJwt.isAdmin],upload.single("file"),subSubjectLecturer.creates);
 app.post('/api/admin/tkb/checkYear', [authJwt.verifyToken, authJwt.isAdmin],subSubjectLecturer.checkYear);
 //app.get('/api/admin/tkb/update',subSubjectLecturer.update);
 
-app.get('/api/admin/report', [authJwt.verifyToken, authJwt.isAdmin],report.list);
+app.get('/api/admin/report', [authJwt.verifyToken, authJwt.isLeaderOrAdmin],report.list);
 
 app.post('/api/auth/signup', [verifySignUp.checkDuplicateUserNameOrEmail, verifySignUp.checkRolesExisted], controller.signup);
 	
 app.post('/api/auth/signin', controller.signin);
-
-app.get('/api/test/user', [authJwt.verifyToken], controller.userContent);
-
-app.get('/api/test/pm', [authJwt.verifyToken, authJwt.isPmOrAdmin], controller.managementBoard);
-
-app.get('/api/test/admin', [authJwt.verifyToken, authJwt.isAdmin], controller.adminBoard);
 
 
 }
